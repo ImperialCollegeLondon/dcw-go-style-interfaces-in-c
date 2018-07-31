9a2ef461b6e59b8bc0f023647d1721c7b223e5f0
dcw <d.white@imperial.ac.uk>
2018-07-31 19:08:14 +0100

    added first example of how I might support function signature checking: a char variable for each function, whose name embedded all the signature info

--- pkg1.c
+++ pkg1.c
@@ -27,3 +27,12 @@ void *pkg1_f4( int n )
 	printf( "pkg1::f4, x=%d, returning NULL\n", n );
 	return NULL;
 }
+
+// attempt at checking function signatures.  Could munge the function
+// name itself, as in C++ type munging, but I wondered if this flag
+// variable per function [never used] is a neater approach, at least
+// when normal use (not via the interface) is concerned
+char pkg1_f1_void_void;
+char pkg1_f2_int_void;
+char pkg1_f3_void_charstar_int;
+char pkg1_f4_voidstart_int;
